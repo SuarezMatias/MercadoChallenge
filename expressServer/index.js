59fa545aa859825f8777d4a99c5bdfba3615a16d
matias suarez <matias_badi@hotmail.com>
2022-02-22 18:34:28 -0300

    First Commit

---
+++ expressServer/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors")
+const request = require("request");
+const app = express();
+
+app.use(cors()); 
+
+const api = "https://api.mercadolibre.com/"
+
+app.get("/api/items", (req, res) => {
+    debugger;
+    request(`${api}/sites/MLA/search?q=${req.query.q}`,(err,response,body)=>{
+        debugger;
+        if (!err){
+            const search = JSON.parse(body);
+            res.send(search)
+        }
+    })
+});
+
+app.get("/api/items/:id", (req, res) => {    
+    request(`${api}/items/${req.params.id}`,(err,response,body)=>{
+        if (!err){
+            const search = JSON.parse(body);
+            res.send(search)           
+        }
+    })
+});
+
+app.get("/api/items/:id/description", (req, res) => {
+    request(`${api}/items/${req.params.id}/description`,(err,response,body)=>{
+        if (!err){
+            const search = JSON.parse(body);
+            res.send(search)           
+        }
+    })
+});
+
+
+app.listen(3000, () => {
+    console.log('Server listening on port', 3000);
+});
